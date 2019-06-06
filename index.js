5f1951ca19f53a1b8101659860b119c49b46362e
Vinicius Oliveira <fukhaos@gmail.com>
2019-06-05 21:57:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
-import React from "react";
-import { StyleSheet, View, Modal, ActivityIndicator } from "react-native";
-import PropTypes from "prop-types";
+import React from 'react';
+import {
+  StyleSheet, View, Modal, ActivityIndicator, Text
+} from 'react-native';
+import PropTypes from 'prop-types';
 
 const Loader = ({
-  loading = false,
-  color,
-  size,
-  opacity = 0.4,
-  title = ""
-}) => {
-  return (
-    <Modal
-      transparent
-      animationType={"none"}
-      visible={loading}
-      onRequestClose={() => null}
-    >
-      <View
-        style={[
-          styles.modalBackground,
-          { backgroundColor: `rgba(0,0,0,${opacity})` }
-        ]}
-      >
-        <View style={styles.activityIndicatorWrapper}>
-          <ActivityIndicator animating={loading} color={color} size={size} />
-          <Text style={styles.title} numberOfLines={1}>
-            {title}
-          </Text>
-        </View>
+  loading = false, color, size, opacity = 0.4, title = ''
+}) => (
+  <Modal transparent animationType="none" visible={loading} onRequestClose={() => null}>
+    <View style={[styles.modalBackground, { backgroundColor: `rgba(0,0,0,${opacity})` }]}>
+      <View style={styles.activityIndicatorWrapper}>
+        <ActivityIndicator animating={loading} color={color} size={size} />
+        <Text style={styles.title} numberOfLines={1}>
+          {title}
+        </Text>
       </View>
-    </Modal>
-  );
-};
+    </View>
+  </Modal>
+);
 
 Loader.propTypes = {
   loading: PropTypes.bool.isRequired,
@@ -39,7 +25,7 @@ Loader.propTypes = {
   size: PropTypes.string,
   opacity: (props, propName, componentName) => {
     if (props[propName] < 0 || props[propName] > 1) {
-      return new Error("Opacity prop value out of range");
+      return new Error('Opacity prop value out of range');
     }
   },
   title: PropTypes.string
@@ -48,19 +34,19 @@ Loader.propTypes = {
 const styles = StyleSheet.create({
   modalBackground: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   activityIndicatorWrapper: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     height: 100,
     width: 100,
     borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   title: {
-    position: "absolute",
+    position: 'absolute',
     paddingTop: 50
   }
 });
